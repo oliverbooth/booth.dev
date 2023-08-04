f2da5f93639c2dc64f0f85b197806eb971a4ef75
Oliver Booth <me@olivr.me>
2023-08-04 12:55:16 +0100

    feat: add ts build pipeline

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,16 +2,27 @@ const gulp = require('gulp');
 const sass = require('gulp-sass')(require('node-sass'));
 const cleanCSS = require('gulp-clean-css');
 const rename = require('gulp-rename');
+const ts = require('gulp-typescript');
+const terser = require('gulp-terser');
 
-const srcDir = 'src/scss'; // Update with your SCSS source directory
-const destDir = 'oliverbooth.dev/wwwroot/css'; // Update with your CSS output directory
+const srcDir = 'src';
+const destDir = 'oliverbooth.dev/wwwroot';
 
 function compileSCSS() {
-    return gulp.src(`${srcDir}/**/*.scss`)
+    return gulp.src(`${srcDir}/scss/**/*.scss`)
         .pipe(sass().on('error', sass.logError))
         .pipe(cleanCSS({ compatibility: 'ie11' }))
         .pipe(rename({ suffix: '.min' }))
-        .pipe(gulp.dest(destDir));
+        .pipe(gulp.dest(`${destDir}/css`));
+}
+
+function compileTS() {
+    return gulp.src(`${srcDir}/ts/**/*.ts`)
+        .pipe(ts())
+        .pipe(terser())
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest(`${destDir}/js`));
 }
 
 exports.default = compileSCSS;
+exports.default = gulp.parallel(compileSCSS, compileTS);
---
+++ src/ts/app.ts
@@ -0,0 +1,6 @@
+﻿declare const hljs: any;
+
+(() => {
+    hljs.highlightAll();
+    document.querySelectorAll("pre code").forEach((e: HTMLElement) => e.style.paddingTop = "0");
+})();
