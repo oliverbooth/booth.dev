cd44b9cf2a32b77281e76480dd2351d825db25ad
Oliver Booth <me@olivr.me>
2023-08-05 20:54:55 +0100

    feat: add layout of contact forms

---
+++ oliverbooth.dev/Pages/Contact/Privacy.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OliverBooth.Pages.Contact;
+
+public class Privacy : PageModel
+{
+    public string Which { get; private set; } = "website";
+
+    public void OnGet(string? which = "website")
+    {
+        Which = which ?? "website";
+    }
+
+    public void SubmitForm()
+    {
+    }
+}
