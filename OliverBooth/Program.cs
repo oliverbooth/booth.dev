b2ada7d720be4d5fe3657058ed85d77bb35879cc
Oliver Booth <me@olivr.me>
2023-08-06 02:27:16 +0100

    fix: amend 75eed18bc81c19da1ddf5ecad7a10cdd79a9b133

--- OliverBooth/Program.cs
+++ OliverBooth/Program.cs
@@ -1,31 +1,9 @@
-using System.Globalization;
-using Microsoft.AspNetCore.Localization;
-using Microsoft.Extensions.Localization;
-using OliverBooth;
-using OliverBooth.Middleware;
-
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
-builder.Services.AddRazorPages().AddViewLocalization().AddDataAnnotationsLocalization();
-
+builder.Services.AddRazorPages();
 builder.Services.AddControllersWithViews();
 builder.Services.AddRouting(options => options.LowercaseUrls = true);
-
-var supportedCultures = new[]
-{
-    CultureInfo.GetCultureInfo("en"),
-    CultureInfo.GetCultureInfo("fr")
-};
-
-builder.Services.Configure<RequestLocalizationOptions>(options =>
-{
-    options.DefaultRequestCulture = new RequestCulture(supportedCultures[0]);
-    options.SupportedCultures = supportedCultures;
-    options.SupportedUICultures = supportedCultures;
-});
-
-builder.Services.Configure<LocalizationOptions>(options => options.ResourcesPath = "Resources");
 
 WebApplication app = builder.Build();
 
@@ -38,22 +16,11 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
 app.UseAuthorization();
 
-app.UseRequestLocalization(options =>
-{
-    options.ApplyCurrentCultureToResponseHeaders = true;
-    options.DefaultRequestCulture = new RequestCulture(supportedCultures[0]);
-    options.SupportedCultures = supportedCultures;
-    options.SupportedUICultures = supportedCultures;
-    options.RequestCultureProviders.Insert(0, new RouteCultureProvider(supportedCultures[0]));
-});
-
 app.MapControllers();
-app.UseCultureRedirect();
-app.MapControllerRoute("default", "{culture=en}/{controller=Home}/{action=Index}/{id?}");
+app.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
 app.MapRazorPages();
 
 app.Run();
