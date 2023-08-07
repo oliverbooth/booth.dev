12309c0bf102cdad336bb4042b1d0b1574d51b14
Oliver Booth <me@olivr.me>
2023-08-08 00:28:33 +0100

    style: improve layout of blog/article

--- OliverBooth/Pages/Blog/Article.cshtml.cs
+++ OliverBooth/Pages/Blog/Article.cshtml.cs
@@ -1,4 +1,4 @@
-﻿using Humanizer;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using OliverBooth.Data;
@@ -15,13 +15,13 @@ public class Article : PageModel
         _dbContextFactory = dbContextFactory;
     }
 
-    public Author? Author { get; private set; }
+    public Author Author { get; private set; }
 
-    public BlogPost? Post { get; private set; }
+    public BlogPost Post { get; private set; } = new();
 
     public string SanitizeContent(string content)
     {
-        content = content.Replace("<more>", string.Empty);
+        content = content.Replace("<!--more-->", string.Empty);
 
         while (content.Contains("\n\n"))
         {
@@ -31,20 +31,22 @@ public class Article : PageModel
         return Markdig.Markdown.ToHtml(content.Trim());
     }
 
-    public void OnGet(int year, int month, string slug)
+    public IActionResult OnGet(int year, int month, int day, string slug)
     {
         using BlogContext context = _dbContextFactory.CreateDbContext();
         Post = context.BlogPosts.FirstOrDefault(p => p.Published.Year == year &&
                                                      p.Published.Month == month &&
-                                                     p.Slug == slug);
+                                                     p.Published.Day == day &&
+                                                     p.Slug == slug)!;
 
+        // ReSharper disable once ConditionIsAlwaysTrueOrFalseAccordingToNullableAPIContract
         if (Post is null)
         {
             Response.StatusCode = 404;
+            return NotFound();
         }
-        else
-        {
-            Author = context.Authors.FirstOrDefault(a => a.Id == Post.AuthorId);
-        }
+
+        Author = context.Authors.FirstOrDefault(a => a.Id == Post.AuthorId)!;
+        return Page();
     }
 }
