2df24a99e7f81ffbb387a2c403ba6fda1cf5f114
Oliver Booth <me@olivr.me>
2023-08-08 02:08:18 +0100

    feat: add bs tooltips

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -1,3 +1,6 @@
+declare const bootstrap: any;
 
 (() => {
+    const list = document.querySelectorAll('[data-bs-toggle="tooltip"]');
+    list.forEach((el: Element) => new bootstrap.Tooltip(el));
 })();
