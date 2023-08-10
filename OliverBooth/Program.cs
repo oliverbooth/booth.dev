8dd4468c1ad01e3cb8e125b1d6c3cae08a909de6
Oliver Booth <me@olivr.me>
2023-08-10 22:49:44 +0100

    feat: gracefully shutdown logmanager on exit

--- OliverBooth/Program.cs
+++ OliverBooth/Program.cs
@@ -1,4 +1,5 @@
 using Markdig;
+using NLog;
 using NLog.Extensions.Logging;
 using OliverBooth.Data;
 using OliverBooth.Markdown;
@@ -52,3 +53,5 @@ app.MapRazorPages();
 app.MapRssFeed("/blog/feed");
 
 app.Run();
+
+LogManager.Shutdown();
