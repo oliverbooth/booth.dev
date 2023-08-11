37a35d5aab42e07ec527526dfc5dfda3d8f3fce6
Oliver Booth <me@olivr.me>
2023-08-11 16:34:10 +0100

    feat: add author id to class

--- src/ts/Author.ts
+++ src/ts/Author.ts
@@ -1,10 +1,16 @@
 class Author {
+    private readonly _id: string;
     private readonly _name: string;
     private readonly _avatarHash: string;
 
     constructor(json: any) {
+        this._id = json.id;
         this._name = json.name;
         this._avatarHash = json.avatarHash;
+    }
+
+    get id(): string {
+        return this._id;
     }
 
     get name(): string {
@@ -15,4 +21,5 @@ class Author {
         return this._avatarHash;
     }
 }
+
 export default Author;
