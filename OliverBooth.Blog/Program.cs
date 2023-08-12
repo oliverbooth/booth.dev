86bbf803b57c78002bd2b2be5dbbf54924e454cb
Oliver Booth <me@olivr.me>
2023-08-12 20:30:10 +0100

    feat: share wwwroot assets

--- OliverBooth.Blog/Program.cs
+++ OliverBooth.Blog/Program.cs
@@ -1,11 +1,13 @@
 using OliverBooth.Blog.Data;
 using OliverBooth.Blog.Middleware;
 using OliverBooth.Blog.Services;
+using OliverBooth.Common;
 using OliverBooth.Common.Extensions;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 builder.Configuration.AddTomlFile("data/config.toml", true, true);
 
+builder.Services.ConfigureOptions<OliverBoothConfigureOptions>();
 builder.Services.AddDbContextFactory<BlogContext>();
 builder.Services.AddSingleton<IBlogPostService, BlogPostService>();
 builder.Services.AddSingleton<IUserService, UserService>();
