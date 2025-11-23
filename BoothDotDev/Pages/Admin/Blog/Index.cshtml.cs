dfb04d13fe2a08085c5ea7061eb18ef01dc95dc7
Oliver Booth <oliver@booth.dev>
2025-11-23 20:31:53 +0000

    feat: add blog post table

---
+++ BoothDotDev/Pages/Admin/Blog/Index.cshtml.cs
@@ -0,0 +1,37 @@
+using BoothDotDev.Common.Data.Blog;
+using BoothDotDev.Common.Services;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BoothDotDev.Pages.Admin.Blog;
+
+/// <summary>
+///     Represents a class which defines the model for the <c>/admin/blog</c> route.
+/// </summary>
+public sealed class Index : PageModel
+{
+    private readonly IBlogPostService _blogPostService;
+
+    /// <summary>
+    ///     Initializes a new instance of the <see cref="Index" /> class.
+    /// </summary>
+    /// <param name="blogPostService">The blog post service.</param>
+    public Index(IBlogPostService blogPostService)
+    {
+        _blogPostService = blogPostService;
+    }
+
+    /// <summary>
+    ///     Gets the blog posts.
+    /// </summary>
+    /// <value>The blog posts.</value>
+    public IReadOnlyList<IBlogPost> BlogPosts { get; private set; } = [];
+
+    /// <summary>
+    ///     Handles the incoming GET request to the page.
+    /// </summary>
+    public void OnGet()
+    {
+        BlogPosts = _blogPostService.GetAllBlogPosts().OrderByDescending(post => post.Published).ToArray();
+    }
+}
+    
