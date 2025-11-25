6aeedac1a9d058f536cc052478454b51b9b17b14
Oliver Booth <oliver@booth.dev>
2025-11-25 16:10:43 +0000

    chore: remove unused dependencies
    - MailKit:4.14.1
    - MailKit.Simplified.Sender:2.12.0
    - NetBarcode:1.8.3
    
    this change also removes the barcode API previously used in /books

--- BoothDotDev/Data/Web/Book.cs
+++ BoothDotDev/Data/Web/Book.cs
@@ -1,8 +1,4 @@
 using BoothDotDev.Common.Data.Web;
-using NetBarcode;
-using SixLabors.ImageSharp;
-using SixLabors.ImageSharp.Formats.Png;
-using SixLabors.ImageSharp.Processing;
 
 namespace BoothDotDev.Data.Web;
 
@@ -22,16 +18,4 @@ internal sealed class Book : IBook
 
     /// <inheritdoc />
     public string Title { get; private set; } = string.Empty;
-
-    public string GetBarcode()
-    {
-        var barcode = new Barcode(Isbn, NetBarcode.Type.EAN13);
-        using var image = barcode.GetImage();
-        int width = image.Width;
-        int height = image.Height;
-        image.Mutate(i => i.Pad(width + 10, height + 10, Color.White));
-        image.Mutate(i => i.Resize(i.GetCurrentSize() / 4 * 3));
-
-        return image.ToBase64String(PngFormat.Instance);
-    }
 }
