00d45f85c1db2c3be2a4f40db6f0855b1a368f0d
Oliver Booth <oliver@booth.dev>
2025-11-30 14:33:27 +0000

    doc: add xmldoc to TocItem

--- BoothDotDev/Data/TocItem.cs
+++ BoothDotDev/Data/TocItem.cs
@@ -1,9 +1,32 @@
 namespace BoothDotDev.Data;
 
+/// <summary>
+///     Represents an item in a table of contents.
+/// </summary>
 public class TocItem
 {
-    public string Text { get; set; } = "";
+    /// <summary>
+    ///     Gets the child items of the table of contents item.
+    /// </summary>
+    /// <value>The child items of the table of contents item.</value>
+    public List<TocItem> Children { get; } = [];
+
+    /// <summary>
+    ///     Gets or sets the level of the table of contents item.
+    /// </summary>
+    /// <value>The level of the table of contents item.</value>
     public int Level { get; set; }
+
+    /// <summary>
+    ///     Gets or sets the ID of the table of contents item.
+    /// </summary>
+    /// <value>The ID of the table of contents item.</value>
     public string Id { get; set; } = "";
-    public List<TocItem> Children { get; } = new List<TocItem>();
+
+
+    /// <summary>
+    ///     Gets or sets the text of the table of contents item.
+    /// </summary>
+    /// <value>The text of the table of contents item.</value>
+    public string Text { get; set; } = "";
 }
