92b152ae2cb533edc9168330d58d441f86737a22
Oliver Booth <oliver@booth.dev>
2026-08-06 18:18:42 +0100

    feat: add real landing page

--- BoothDotDev/Pages/Index.cshtml.cs
+++ BoothDotDev/Pages/Index.cshtml.cs
@@ -1,12 +1,30 @@
-using Microsoft.AspNetCore.Mvc;
+using BoothDotDev.Common.Data.Models;
+using BoothDotDev.Common.Services;
+using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace BoothDotDev.Pages;
 
-internal sealed class Index : PageModel
+public partial class Index : PageModel
 {
-    public IActionResult OnGet()
+    private readonly IBlogPostService _blogPostService;
+
+    public Index(IBlogPostService blogPostService)
     {
-        return RedirectToPage("/Blog/Index");
+        _blogPostService = blogPostService;
+    }
+
+    /// <summary>
+    ///     Gets the latest blog posts.
+    /// </summary>
+    /// <returns>The latest blog posts.</returns>
+    public IReadOnlyList<IBlogPost> BlogPosts { get; private set; } = [];
+
+    /// <summary>
+    ///     Handles the GET request for the index page.
+    /// </summary>
+    public void OnGet()
+    {
+        BlogPosts = _blogPostService.GetBlogPosts(0, 3);
     }
 }
