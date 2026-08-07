50fe07ac57d8a34e6c89ef6331110fc5b061ab85
Oliver Booth <oliver@booth.dev>
2026-08-07 14:54:11 +0100

    fix: add missing extension methods

---
+++ BoothDotDev/Extensions/BlogPostExtensions.cs
@@ -0,0 +1,53 @@
+using System.Buffers;
+using BoothDotDev.Data.Models;
+
+namespace BoothDotDev.Extensions;
+
+/// <summary>
+///     Extension methods for <see cref="BlogPost"/>.
+/// </summary>
+internal static class BlogPostExtensions
+{
+    private const int WordsPerMinute = 275;
+
+    private static readonly SearchValues<char> WhitespaceChars = SearchValues.Create(
+        "\t\n\v\f\r " +
+        "\u0085\u00A0\u1680\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200A\u2028\u2029\u202F\u205F\u3000"
+    );
+
+    /// <param name="post">The <see cref="BlogPost"/>.</param>
+    extension(BlogPost post)
+    {
+        /// <summary>
+        ///     Returns the estimated reading time of the blog post, in minutes.
+        /// </summary>
+        /// <returns>The estimated reading time of the blog post, in minutes.</returns>
+        public int GetEstimatedReadingTime()
+        {
+            var wordCount = CountWords(post.Body);
+            return Math.Max(1, wordCount / WordsPerMinute);
+        }
+    }
+
+    private static int CountWords(ReadOnlySpan<char> text)
+    {
+        var wordCount = 0;
+
+        while (!text.IsEmpty)
+        {
+            var nonWhitespaceStart = text.IndexOfAnyExcept(WhitespaceChars);
+            if (nonWhitespaceStart < 0)
+            {
+                break; // rest is all whitespace
+            }
+
+            text = text[nonWhitespaceStart..];
+            wordCount++;
+
+            var nextWhitespace = text.IndexOfAny(WhitespaceChars);
+            text = nextWhitespace < 0 ? [] : text[nextWhitespace..];
+        }
+
+        return wordCount;
+    }
+}
