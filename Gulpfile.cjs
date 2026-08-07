20d9cbd102c0a2b92e6131470716ec2a26640b73
Oliver Booth <oliver@booth.dev>
2026-08-07 14:58:03 +0100

    chore: emit sourcemaps

--- Gulpfile.cjs
+++ Gulpfile.cjs
@@ -2,6 +2,7 @@ const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const cleanCSS = require('gulp-clean-css');
 const rename = require('gulp-rename');
+const sourcemaps = require('gulp-sourcemaps');
 const ts = require('gulp-typescript');
 const terser = require('gulp-terser');
 const webpack = require('webpack-stream');
@@ -16,39 +17,53 @@ async function clean() {
 
 function compileSCSS() {
     return gulp.src(`${srcDir}/scss/**/*.scss`)
+        .pipe(sourcemaps.init())
         .pipe(sass().on('error', sass.logError))
         .pipe(cleanCSS({ compatibility: 'ie11' }))
         .pipe(rename({ suffix: '.min' }))
+        .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest(`${destDir}/css`));
 }
 
 function compileTS() {
     return gulp.src(`${srcDir}/ts/**/*.ts`)
+        .pipe(sourcemaps.init())
         .pipe(ts("tsconfig.json"))
         .pipe(terser())
+        .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest(`tmp/js`));
 }
 
 function bundleJS() {
-    return gulp.src('tmp/js/*.js')
-        .pipe(webpack({ mode: 'production', output: { filename: 'app.min.js' } }))
+    return gulp.src('tmp/js/*.js', { sourcemaps: true })
+        .pipe(webpack({
+            mode: 'production',
+            output: { filename: 'app.min.js' },
+            devtool: 'source-map'
+        }))
+        .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest(`${destDir}/js`));
 }
 
 function copyJS() {
     return gulp.src(`${srcDir}/ts/**/*.js`)
+        .pipe(sourcemaps.init())
         .pipe(rename({ suffix: '.min' }))
+        .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest(`${destDir}/js`));
 }
 
 function copyCSS() {
     return gulp.src(`${srcDir}/scss/**/*.css`)
         .pipe(rename({ suffix: '.min' }))
+        .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest(`${destDir}/css`));
 }
 
 function copyImages() {
     return gulp.src(`${srcDir}/img/**/*.*`, { encoding: false })
+        .pipe(sourcemaps.init())
+        .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest(`${destDir}/img`));
 }
 
