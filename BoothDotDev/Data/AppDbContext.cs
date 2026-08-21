58963eabf157bcc944a25965076b0a66e9670421
Oliver Booth <oliver@booth.dev>
2026-08-21 23:59:56 +0100

    refactor: compute DbSet properties

--- BoothDotDev/Data/AppDbContext.cs
+++ BoothDotDev/Data/AppDbContext.cs
@@ -16,126 +16,160 @@ public sealed class AppDbContext : DbContext
     /// <param name="options">The options for creating a new context.</param>
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
     {
-        ArtworkItems = Set<ArtworkItem>();
-        BlogPostCategories = Set<BlogPostCategory>();
-        BlogPostDrafts = Set<BlogPostDraft>();
-        BlogPosts = Set<BlogPost>();
-        Books = Set<Book>();
-        CodeSnippets = Set<CodeSnippet>();
-        DevChallenges = Set<DevChallenge>();
-        DevLogs = Set<ProjectDevlog>();
-        LegacyComments = Set<LegacyComment>();
-        MusicItems = Set<MusicItem>();
-        NoteDrafts = Set<NoteDraft>();
-        Notes = Set<Note>();
-        Projects = Set<Project>();
-        SiteConfiguration = Set<SiteConfiguration>();
-        TutorialArticles = Set<TutorialArticle>();
-        TutorialFolders = Set<TutorialFolder>();
-        Users = Set<User>();
     }
 
     /// <summary>
     ///     Gets the collection of artwork items in the database.
     /// </summary>
     /// <value>The collection of artwork items.</value>
-    public DbSet<ArtworkItem> ArtworkItems { get; private set; }
+    public DbSet<ArtworkItem> ArtworkItems
+    {
+        get => Set<ArtworkItem>();
+    }
 
     /// <summary>
     ///     Gets the collection of blog post categories in the database.
     /// </summary>
     /// <value>The collection of blog post categories.</value>
-    public DbSet<BlogPostCategory> BlogPostCategories { get; private set; }
+    public DbSet<BlogPostCategory> BlogPostCategories
+    {
+        get => Set<BlogPostCategory>();
+    }
 
     /// <summary>
     ///     Gets the collection of blog post drafts in the database.
     /// </summary>
     /// <value>The collection of blog post drafts.</value>
-    public DbSet<BlogPostDraft> BlogPostDrafts { get; private set; }
+    public DbSet<BlogPostDraft> BlogPostDrafts
+    {
+        get => Set<BlogPostDraft>();
+    }
 
     /// <summary>
     ///     Gets the collection of blog posts in the database.
     /// </summary>
     /// <value>The collection of blog posts.</value>
-    public DbSet<BlogPost> BlogPosts { get; private set; }
+    public DbSet<BlogPost> BlogPosts
+    {
+        get => Set<BlogPost>();
+    }
 
     /// <summary>
     ///     Gets the collection of books in the reading list.
     /// </summary>
     /// <value>The collection of books.</value>
-    public DbSet<Book> Books { get; private set; }
+    public DbSet<Book> Books
+    {
+        get => Set<Book>();
+    }
 
     /// <summary>
     ///     Gets the collection of code snippets in the database.
     /// </summary>
     /// <value>The collection of code snippets.</value>
-    public DbSet<CodeSnippet> CodeSnippets { get; private set; }
+    public DbSet<CodeSnippet> CodeSnippets
+    {
+        get => Set<CodeSnippet>();
+    }
 
     /// <summary>
     ///     Gets the collection of dev challenges in the database.
     /// </summary>
     /// <value>The collection of dev challenges.</value>
-    public DbSet<DevChallenge> DevChallenges { get; private set; }
+    public DbSet<DevChallenge> DevChallenges
+    {
+        get => Set<DevChallenge>();
+    }
 
     /// <summary>
     ///     Gets the collection of project devlogs in the database.
     /// </summary>
     /// <value>The collection of project devlogs.</value>
-    public DbSet<ProjectDevlog> DevLogs { get; private set; }
+    public DbSet<ProjectDevlog> DevLogs
+    {
+        get => Set<ProjectDevlog>();
+    }
 
     /// <summary>
     ///     Gets the collection of legacy comments in the database.
     /// </summary>
     /// <value>The collection of legacy comments.</value>
-    public DbSet<LegacyComment> LegacyComments { get; private set; }
+    public DbSet<LegacyComment> LegacyComments
+    {
+        get => Set<LegacyComment>();
+    }
 
     /// <summary>
     ///     Gets the collection of music items in the database.
     /// </summary>
     /// <value>The collection of music items.</value>
-    public DbSet<MusicItem> MusicItems { get; private set; }
+    public DbSet<MusicItem> MusicItems
+    {
+        get => Set<MusicItem>();
+    }
 
     /// <summary>
     ///     Gets the collection of note drafts in the database.
     /// </summary>
     /// <value>The collection of note drafts.</value>
-    public DbSet<NoteDraft> NoteDrafts { get; private set; }
+    public DbSet<NoteDraft> NoteDrafts
+    {
+        get => Set<NoteDraft>();
+    }
 
     /// <summary>
     ///     Gets the collection of notes in the database.
     /// </summary>
     /// <value>The collection of notes.</value>
-    public DbSet<Note> Notes { get; private set; }
+    public DbSet<Note> Notes
+    {
+        get => Set<Note>();
+    }
 
     /// <summary>
     ///     Gets the collection of projects in the database.
     /// </summary>
     /// <value>The collection of projects.</value>
-    public DbSet<Project> Projects { get; private set; }
+    public DbSet<Project> Projects
+    {
+        get => Set<Project>();
+    }
 
     /// <summary>
     ///     Gets the set of site configuration items.
     /// </summary>
     /// <value>The set of site configuration items.</value>
-    public DbSet<SiteConfiguration> SiteConfiguration { get; private set; }
+    public DbSet<SiteConfiguration> SiteConfiguration
+    {
+        get => Set<SiteConfiguration>();
+    }
 
     /// <summary>
     ///     Gets the collection of tutorial articles in the database.
     /// </summary>
     /// <value>The collection of tutorial articles.</value>
-    public DbSet<TutorialArticle> TutorialArticles { get; private set; }
+    public DbSet<TutorialArticle> TutorialArticles
+    {
+        get => Set<TutorialArticle>();
+    }
 
     /// <summary>
     ///     Gets the collection of tutorial folders in the database.
     /// </summary>
     /// <value>The collection of tutorial folders.</value>
-    public DbSet<TutorialFolder> TutorialFolders { get; private set; }
+    public DbSet<TutorialFolder> TutorialFolders
+    {
+        get => Set<TutorialFolder>();
+    }
 
     /// <summary>
     ///     Gets the collection of users in the database.
     /// </summary>
     /// <value>The collection of users.</value>
-    public DbSet<User> Users { get; private set; }
+    public DbSet<User> Users
+    {
+        get => Set<User>();
+    }
 
     /// <inheritdoc />
     protected override void OnModelCreating(ModelBuilder modelBuilder)
